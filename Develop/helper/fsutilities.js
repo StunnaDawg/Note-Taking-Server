d8a3a29e48143cd3f975b80a4d2b7bb0912ba16d
StunnaDawg <jonsonallen9@gmail.com>
2023-03-31 15:54:18 -0300

    helper fs functions complete

--- Develop/helper/fsutilities.js
+++ Develop/helper/fsutilities.js
@@ -1,9 +1,28 @@
+const { json } = require('express');
 const fs = require('fs');
 const util = require('util');
 
+// promsify turns fs.readfile into a promise function so it will wait its turn
 const readFromFile = util.promisify(fs.readFile);
 
+// content is the content within the object that is to be written
+// adding null means all values within the object will be stringified
+// 4 is just for indentation
 const writeToFile = (destination, content) => {
     fs.writeFile(destination, JSON.stringify(content, null, 4) , (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`))
 }
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    })
+}
+
+module.exports = { readFromFile, writeToFile, readAndAppend };
---
+++ Develop/server/notes.js
@@ -0,0 +1,1 @@
+const { readFromFile, writeToFile, readAndAppend } = require('../helper/fsutilities');

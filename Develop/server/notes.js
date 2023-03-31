98aa2dda846bd0c6aaaefc0cf210035ab17d7499
StunnaDawg <jonsonallen9@gmail.com>
2023-03-31 16:11:01 -0300

    index starting route add

--- Develop/server/notes.js
+++ Develop/server/notes.js
@@ -1,1 +1,9 @@
+const notes = require('express').Router()
 const { readFromFile, writeToFile, readAndAppend } = require('../helper/fsutilities');
+
+notes.get('/', (req, res) => {
+    console.info(`${req.method} recieved for the stored notes`)
+    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)))
+})
+
+module.exports = {notes};

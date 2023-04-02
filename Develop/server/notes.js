7bbdf343dac4e3528e81bf9da55b93b6bfe9e578
StunnaDawg <jonsonallen9@gmail.com>
2023-04-02 12:31:04 -0300

    post method added

--- Develop/server/notes.js
+++ Develop/server/notes.js
@@ -6,4 +6,23 @@ notes.get('/', (req, res) => {
     readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)))
 })
 
+notes.post('/', (req, res) => {
+    console.info(`${req.method} request complete`)
+    console.log(`${req.body}`);
+
+    const {title, text} = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text
+        }
+    
+    readAndAppend(newNote, '../db/db.json');
+    res.json('Note recieved')
+    } else {
+        res.error('error error')
+    }
+})
+
 module.exports = notes;
